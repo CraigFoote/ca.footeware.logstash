a014e907535281ee5aedd1918393e1f3bafa9483
craig <craig@notebook>
2018-06-14 19:41:53 -0400

    Use ProgressMonitor for folder and files.

--- ca.footeware.logstash/src/ca/footeware/logstash/wizards/NewLogstashConfigWizard.java
+++ ca.footeware.logstash/src/ca/footeware/logstash/wizards/NewLogstashConfigWizard.java
@@ -6,6 +6,7 @@ package ca.footeware.logstash.wizards;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 
 import org.eclipse.core.resources.IFile;
@@ -17,12 +18,21 @@ import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ISetSelectionTarget;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
@@ -90,41 +100,70 @@ public class NewLogstashConfigWizard extends Wizard implements INewWizard {
 		String host = pageOne.getHost();
 		String jira = pageOne.getJira();
 
-		UserInput input = new UserInput();
-		input.setName(name);
-		input.setPort(port);
-		input.setHost(host);
-		input.setJira(jira);
-
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		IWorkspaceRoot root = workspace.getRoot();
-		IProject project = root.getProject(parent.getName());
-		IFolder folder = project.getFolder(name);
-
 		try {
-			folder.create(true, true, null);
-		} catch (CoreException e) {
+			// create files in new thread with progress monitor for user
+			getContainer().run(true, false, new IRunnableWithProgress() {
+				@Override
+				public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+					monitor.beginTask("Creating folder and files.", 5);
+					
+					UserInput input = new UserInput();
+					input.setName(name);
+					input.setPort(port);
+					input.setHost(host);
+					input.setJira(jira);
+					monitor.worked(1);
+					
+					IWorkspace workspace = ResourcesPlugin.getWorkspace();
+					IWorkspaceRoot root = workspace.getRoot();
+					IProject project = root.getProject(parent.getName());
+					IFolder folder = project.getFolder(name);
+					monitor.worked(1);
+					
+					try {
+						folder.create(true, true, null);
+					} catch (CoreException e) {
+						throw new InvocationTargetException(e);
+					}
+					monitor.worked(1);
+
+					FreemarkerTemplateEngine engine = new FreemarkerTemplateEngine();
+					engine.init("templates"); // template location
+					monitor.worked(1);
+					
+					IFile log4j = folder.getFile("log4j2.properties");
+					try (Writer fileWriter = new FileWriter(log4j.getRawLocation().makeAbsolute().toFile())) {
+						engine.setTemplate("log4j2.properties"); // .ftl
+						engine.process(fileWriter, input);
+						engine.flush();
+						monitor.worked(1);
+					} catch (IOException | TemplateException e) {
+						throw new InvocationTargetException(e);
+					}
+
+					// update the UI
+					Display.getDefault().asyncExec(new Runnable() {
+						@Override
+						public void run() {
+							try {
+								parent.refreshLocal(IResource.DEPTH_INFINITE, null);
+							} catch (CoreException e) {
+								pageOne.setErrorMessage(e.getMessage());
+							}
+							IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+							IViewPart view = page.findView(IPageLayout.ID_PROJECT_EXPLORER);
+							// select logstash.conf file, the one that needs editing
+							((ISetSelectionTarget) view).selectReveal(new StructuredSelection(log4j));
+						}
+					});
+					monitor.done();
+				}
+			});
+		} catch (InterruptedException e) {
 			pageOne.setErrorMessage(e.getMessage());
 			return false;
-		}
-
-		FreemarkerTemplateEngine engine = new FreemarkerTemplateEngine();
-		engine.init("templates"); // template location
-		
-		IFile log4j = folder.getFile("log4j2.properties");
-		try (Writer fileWriter = new FileWriter(log4j.getRawLocation().makeAbsolute().toFile())) {
-			engine.setTemplate("log4j2.properties"); // .ftl
-			engine.process(fileWriter, input);
-			engine.flush();
-		} catch (IOException | TemplateException e) {
-			pageOne.setErrorMessage(e.getMessage());
-			return false;
-		}
-
-		try {
-			parent.refreshLocal(IResource.DEPTH_INFINITE, null);
-		} catch (CoreException e) {
-			pageOne.setErrorMessage(e.getMessage());
+		} catch (InvocationTargetException e) {
+			pageOne.setErrorMessage(e.getTargetException().getMessage());
 			return false;
 		}
 
